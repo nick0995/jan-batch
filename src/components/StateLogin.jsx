4ff1b9d20ca7185396ab31235740f268d319f825
nick0995 <neeraj.chauhan.49@gmail.com>
2024-01-24 18:27:24 +0530

    forms

---
+++ src/components/StateLogin.jsx
@@ -0,0 +1,51 @@
+import { useRef } from "react"; 
+
+export default function StateLogin() {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+//   let password =  passwordRef.current.value
+    // emailRef <---> input --> email
+    //input -- type krte hai --> onchange --> event --> target.value
+  function handleSubmit(event) {
+    event.preventDefault();
+    const enteredEmail = emailRef.current.type;
+    console.log(enteredEmail);
+    email.current.value = '';
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input 
+            id="email" 
+            type="email" 
+            name="email" 
+            ref={emailRef}
+          />
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input 
+            id="password" 
+            type="password" 
+            name="password" 
+            ref={passwordRef}
+          />
+        </div>
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat">Reset</button>
+        <button className="button"> Login </button>
+      </p>
+    </form>
+  );
+}
+
+// ref
