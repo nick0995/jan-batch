b6f1cb177ecca6d0b7b7482454bdad6a72b94b26
nick0995 <neeraj.chauhan.49@gmail.com>
2024-01-27 18:14:46 +0530

    no needed

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,18 @@ import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import { sortPlacesByDistance } from './loc.js'
 
+const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || []
+const storedPlaces = storedIds.map((id) => 
+  AVAILABLE_PLACES.find((place) => place.id === id)
+)
+
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
   const [availablePlaces, setAvailabalePlaces] = useState([]);
   console.log('EXECUTING APP!');
+
   
   useEffect(()=>{
     console.log('inside useEffect');
@@ -45,6 +51,15 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    let storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || []
+
+    if (storedIds.indexOf(id) === -1) {
+       localStorage.setItem(
+        'selectedPlaces', 
+        JSON.stringify([id,...storedIds])
+      )
+    }
   }
 
   function handleRemovePlace() {
