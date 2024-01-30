170a27b5aeaa1e41057fe6fc47d1e642177c04ea
nick0995 <neeraj.chauhan.49@gmail.com>
2024-01-30 17:35:30 +0530

    ctx project

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,8 +2,8 @@ import { useContext } from 'react';
 
 import { CartContext } from '../store/shopping-cart-context';
 
-export default function Cart({ onUpdateItemQuantity }) {
-  const { items } = useContext(CartContext);
+export default function Cart() {
+  const { items, onUpdateItemQuantity } = useContext(CartContext);
 
   const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
