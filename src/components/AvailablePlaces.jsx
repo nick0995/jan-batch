c14f82280e7211a34c0bf371daeed9a579ed0b6a
nick0995 <neeraj.chauhan.49@gmail.com>
2024-02-01 11:18:22 +0530

    update

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,30 +1,52 @@
-import { useEffect, useState } from 'react';
-import Places from './Places.jsx';
-
+import { useEffect, useState } from "react";
+import Places from "./Places.jsx";
+import Error from "./Error.jsx";
+import { fetchAvailablePlaces } from '../http.js'
+import { sortPlacesByDistance } from "../loc.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
 
   useEffect(() => {
-    async function fetchPlaces(){
+    async function fetchPlaces() {
       setIsFetching(true);
-      const response = await fetch('http://localhost:3000/places');
-      const result = await response.json();
-      setAvailablePlaces(result.places);
+      try {
+        const places = await fetchAvailablePlaces();
+
+        navigator.geolocation.getCurrentPosition((position) =>{
+          const sortedPlaces = sortPlacesByDistance(
+            places,
+            position.coords.latitude,
+            position.coords.longitude
+          )
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
+      } catch (error) {
+        setError({
+          message:
+            error.message || 'Could not fetch places, please try again!'
+        });
+      }
+
       setIsFetching(false);
     }
 
     fetchPlaces();
-    
-  }, [])
-  
+  }, []);
+
+  if(error){
+    return <Error title="An error occurred!" message={error.message} />
+  }
+
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
       isLoading={isFetching}
-      loadingText= "Fetching place data..."
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
